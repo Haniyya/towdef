9c79c64b434d0ef1475346ffeb6e2b805ad11cfa
Paul Martensen <paul.martensen@gmx.de>
2018-08-13 21:11:05 +0200

    Added luacheck standard and some ecs.

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
+local love = require "love_standard"
 local _ = require "lib/moses"
+local engine, system, entity, component = require "lib/adorbs" ()
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
