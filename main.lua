3ad96c94c783acc98404639d9cba9ffa5bb433e4
Andre Sattler <andresattler.webdev@gmail.com>
2018-08-14 23:59:17 +0200

    removed unused code

--- main.lua
+++ main.lua
@@ -2,35 +2,21 @@ local tiny = require("lib/tiny")
 local drawSystem = require("src/systems/draw")
 local thingEntity = require("src/entities/thing")
 
-game = {
-    window = {
-        width = 0,
-        height = 0
-    },
-    
-    view = {
-        width = 180,
-        height = 320
-    }
-}
+local world = {}
 
 -- Load
 function love.load()
-    love.graphics.setDefaultFilter("nearest", "nearest")
-    game.world = initializeECSWorld()
+    world = initializeECSWorld()
 end
 
 -- Update
 function love.update(delta_time)
-    game.world:update(delta_time)
+    world:update(delta_time)
 end
 
 -- Draw
 function love.draw()
-    love.graphics.push()
-    love.graphics.scale(1/.5, 1/.5)
-    draw:update()
-    love.graphics.pop()
+    drawSystem:update()
 end
 
 -- Initialize the ECS world
